98a0662b69c3008d7ae2782ef31ec2916025db4f
Daniel Haro Contreras <daniel@appfluence.com>
2021-12-12 20:51:51 +0100

    Added basic bot functionality and connection to db

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,7 @@ COPY yarn.lock ./
 RUN yarn --frozen-lockfile
 
 COPY . .
+RUN ["yarn", "run", "prisma", "generate"]
+RUN ["yarn", "run", "prisma", "migrate", "deploy"]
 RUN ["chmod", "+x", "./scripts/start.sh"]
 CMD ./scripts/start.sh
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import {Bot, Context, session} from "grammy";
 import { I18n, I18nContext } from "@grammyjs/i18n";
 import dotenv from "dotenv";
+import prismaClient from "./prisma/PrismaClient";
 
 dotenv.config();
 
@@ -20,7 +21,28 @@ const bot = new Bot<MyContext>(process.env.BOT_TOKEN);
 bot.use(session());
 bot.use(i18n.middleware());
 
+bot.api.setMyCommands([
+  { command: "start", description: "Start the bot" },
+  { command: "list", description: "List words" },
+  { command: "add", description: "Add a word" },
+]);
+
 bot.command("start", (ctx) => ctx.reply(ctx.i18n.t("hey")));
+
+bot.command("list", async (ctx) => {
+  const words = await prismaClient.word.findMany();
+  const wordList = words.map(word => `- ${word.word}`);
+  ctx.reply('Word list:\n' + wordList.join("\n"));
+});
+
+bot.command("add", async (ctx) => {
+  const word = ctx.match;
+  await prismaClient.word.create({
+    data: { word }
+  })
+  ctx.reply(`Created word "${word}"`)
+})
+
 bot.on("message", (ctx) => {
   const message = ctx.message;
   ctx.reply(`${ctx.i18n.t("received")}: ${message.text}`);
